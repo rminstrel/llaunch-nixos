7f2578dda280f4a66316382419f16d2ea7955b83
Alif Al Amin <alifalamin586@gmail.com>
2025-01-28 12:00:43 +0600

    Fixed flake.nix

---
+++ README.md
@@ -0,0 +1,5 @@
+# Legacy Launcher on NixOS
+
+This repository contains the Nix flakes required to install Legacy Launcher on a NixOS installation. Legacy Launcher is a simple and lightweight Minecraft launcher, created by turikhay.
+
+To install Legacy Launcher on NixOS follow the given instructions: Coming soon...
--- flake.nix
+++ flake.nix
@@ -1,19 +1,15 @@
 {
   description = "Legacy Minecraft Launcher in a Nix flake";
-
-  # Nixpkgs is the main package set we'll use
+  
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # You can adjust this to the specific version of nixpkgs you're using
-    flake-utils.url = "github:numtide/flake-utils";   # Optional for easier flake management
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-
-  # The outputs function is where we define how the flake is used
+  
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-    in rec {
-      packages = {
-        legacylauncher = import ./default.nix { inherit pkgs; };
-      };
+    in {
+      packages.default = import ./default.nix { inherit pkgs; }; # Changed this line
     });
 }
